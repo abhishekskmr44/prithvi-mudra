02d038bcea0ebe80c790e6bb367f36ac30f381d5
abhishekskmr44 <abhishekskmr44@gmail.com>
2022-07-10 10:29:06 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Do Prithivi Mudra for strong hair, good sleep, fight fever. Strong body etc.
+    </div>
+  );
+}
+
+export default App;
